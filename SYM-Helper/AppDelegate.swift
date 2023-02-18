65960416d379d05d057c5a2abcc3b8aa210c0095
Leslie Helou <leslie@ccs-indy.com>
2023-02-18 15:10:54 -0500

    Initial Commit

---
+++ SYM-Helper/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SYM-Helper
+//
+//  Created by Leslie Helou on 2/18/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ SYM-Helper/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SYM-Helper
+//
+//  Created by Leslie Helou on 2/18/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
