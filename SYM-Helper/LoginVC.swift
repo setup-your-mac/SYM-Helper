a8a2774ccbc12e3922c121e1dcee604b13fbf47b
leslie helou <leslie@ccs-indy.com>
2024-01-21 16:21:30 -0500

    cleanup

--- SYM-Helper/LoginVC.swift
+++ SYM-Helper/LoginVC.swift
@@ -159,10 +159,10 @@ class LoginVC: NSViewController, URLSessionDelegate, NSTextFieldDelegate {
         } else {
             theSender = sender as! String
         }
-        print("[login_action] sender: \(theSender)")
-        print("[login_action] destination: \(JamfProServer.destination)")
-        print("[login_action] username: \(JamfProServer.username)")
-        print("[login_action] userpass: \(JamfProServer.password)")
+//        print("[login_action] sender: \(theSender)")
+//        print("[login_action] destination: \(JamfProServer.destination)")
+//        print("[login_action] username: \(JamfProServer.username)")
+//        print("[login_action] userpass: \(JamfProServer.password)")
         
         // check for update/removal of server display name
         if jamfProServer_textfield.stringValue == "" {
@@ -459,10 +459,6 @@ class LoginVC: NSViewController, URLSessionDelegate, NSTextFieldDelegate {
         JamfProServer.destination = jamfProServer_textfield.stringValue
         JamfProServer.username    = jamfProUsername_textfield.stringValue
         JamfProServer.password    = jamfProPassword_textfield.stringValue
-        
-        print("[credentialsCheck] destination: \(JamfProServer.destination)")
-        print("[credentialsCheck] username: \(JamfProServer.username)")
-        print("[credentialsCheck] userpass: \(JamfProServer.password)")
         
     }
     
